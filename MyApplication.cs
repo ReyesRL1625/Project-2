7af6a824f8962cd422a51635787090a475d38b16
Zadig Alvarez <zadiginiesta@gmail.com>
2019-09-27 14:24:33 -0700

    Travel agency unchanged, and My Application added print statements

--- MyApplication.cs
+++ MyApplication.cs
@@ -6,12 +6,43 @@ namespace Project_2
     class MyApplication
     {
         public static MultiCellBuffer buffer;
+        public static Int32 NumberOfTravelAgencies = 5;
+
         static void Main(string[] args)
         {
-            Airline airline1 = new Airline();
+            //create a multicell buffer
             buffer = new MultiCellBuffer();
+            Console.WriteLine("Creating Two Airline Objects");
+            Airline airline1 = new Airline();
+            Airline airline2 = new Airline();
+            Console.WriteLine("Finished Creating The Two Airline Objects");
+
+            Console.WriteLine("Creating Two Airline Threads");
+            Thread airline1T = new Thread(new ThreadStart(airline1.airlineFunc));
+            Thread airline2T = new Thread(new ThreadStart(airline2.airlineFunc));
+
+            Console.WriteLine("Starting Two Airline Threads");
+            airline1T.Start();
+            airline2T.Start();
+
             //5 TravelAgency Threads
+            Console.WriteLine("Creating a travel agency object");
+            TravelAgency agency = new TravelAgency();
+            Console.WriteLine("Subscribing the travel agency object to the event");
+            Airline.priceCut += new priceCutEvent(agency.ticketsOnSale);
+            Console.WriteLine("Creating 5 travel agency threads and starting them");
             Thread[] travelAgency = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                travelAgency[i] = new Thread(new ThreadStart(agency.travelAgencyFunc));
+                travelAgency[i].Name = (i + 1).ToString();
+                travelAgency[i].Start();
+                Console.WriteLine("Travel Agency {0} started", travelAgency[i].Name);
+            }
         }
+        
+        
+            
+        
     }
 }
--- TravelAgency.cs
+++ TravelAgency.cs
@@ -7,6 +7,8 @@ namespace Project_2
 {
     class TravelAgency
     {
+        private Int32 ticketsNeeded = 200;
+        private Int32 ticketsBought = 0;
         public void travelAgencyFunc()
         {   //for starting thread
             Airline airline = new Airline();
@@ -14,6 +16,8 @@ namespace Project_2
             {
                 Thread.Sleep(500);
                 Int32 p = airline.getPrice();
+                //buy tickets
+                //create an order
                 Console.WriteLine("Travel Agency{0} has everyday low price: ${1} each", Thread.CurrentThread.Name, p);
             }
         }
